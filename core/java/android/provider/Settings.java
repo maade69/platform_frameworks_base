GrapheneOS-Archive/platform_frameworks_base-old

Add infrastructure for choosing PSDS server
This is currently specific to 6th generation Pixels but is intended to
become more generic in the future.

core/java/android/provider/Settings.java
@@ -16380,6 +16380,12 @@ public final class Settings {
          * @hide
          */
         public static final String ATTEST_REMOTE_PROVISIONER_SERVER = "attest_remote_provisioner_server";
+
+        /**
+         * Which server to use for Broadcom GNSS almanacs
+         * @hide
+         */
+        public static final String GNSS_SERVER = "gnss_server";
     }
 
     /**

maade69/platform_frameworks_base

core/java/android/provider/Settings.java
@@ -16434,6 +16434,12 @@ public final class Settings {
          * @hide
          */
         public static final String ATTEST_REMOTE_PROVISIONER_SERVER = "attest_remote_provisioner_server";
+
+        /**
+         * Which server to use for Broadcom GNSS almanacs
+         * @hide
+         */
+        public static final String PSDS_SERVER = "psds_server";
     }
 
     /**