6b3d83577274b242fd80232f1c476d876a22f956
maade69 <70593890+maade69@users.noreply.github.com>
2023-09-29 16:12:53 +0300

    Widevine provisioning setting
    Signed-off-by: maade69 <70593890+maade69@users.noreply.github.com>

--- core/java/android/ext/settings/ExtSettings.java
+++ core/java/android/ext/settings/ExtSettings.java
@@ -14,6 +14,8 @@ import static android.ext.settings.GnssConstants.SUPL_SERVER_GRAPHENEOS_PROXY;
 import static android.ext.settings.GnssConstants.SUPL_SERVER_STANDARD;
 import static android.ext.settings.RemoteProvisioningConstants.GRAPHENEOS_PROXY;
 import static android.ext.settings.RemoteProvisioningConstants.STANDARD_SERVER;
+import static android.ext.settings.WidevineProvisioningConstants.WV_GRAPHENEOS_PROXY;
+import static android.ext.settings.WidevineProvisioningConstants.WV_STANDARD_SERVER;
 
 /** @hide */
 public class ExtSettings {
@@ -125,6 +127,12 @@ public class ExtSettings {
     public static final BoolSysProperty NATIVE_DEBUGGING = new BoolSysProperty(
             "persist.native_debug", true);
 
+    public static final IntSetting WIDEVINE_PROVISIONING_SERVER = new IntSetting(
+            Setting.Scope.GLOBAL, "widevine_provisioner_server", // historical setting key
+            WV_GRAPHENEOS_PROXY, // default
+            WV_STANDARD_SERVER, WV_GRAPHENEOS_PROXY // valid values
+    ); 
+   
     private ExtSettings() {}
 
     // used for making settings defined in this class unreadable by third-party apps
---
+++ core/java/android/ext/settings/WidevineProvisioningConstants.java
@@ -0,0 +1,10 @@
+package android.ext.settings;
+
+/** @hide */
+public interface WidevineProvisioningConstants {
+    int WV_GRAPHENEOS_PROXY = 0;
+    int WV_STANDARD_SERVER = 1;
+    int DISABLED = 2; // unused for now
+
+    String WV_GRAPHENEOS_PROXY_URL = "attestation.maade.dev";
+}
